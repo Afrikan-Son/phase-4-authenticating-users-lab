95c67203b1abb657897f7c4895f9073954e42b47
Mark Wanjau <theafrikanson@gmail.com>
2023-07-07 03:57:42 +0300

    make changes to rooute and finish lab

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+
+    def show
+        user =  User.find(session[:user_id])
+        if user
+            render json: user, status: :ok
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+    
+end
